c81bd1864abcffc048ced2f1ef426440b7351ec9
satyam9k <75788300+satyam9k@users.noreply.github.com>
2024-02-14 16:40:01 +0530

    Update p.py

--- p.py
+++ p.py
@@ -7,7 +7,7 @@ def main():
     )
 
     st.markdown(
-        "<h1 style='text-align: center; color: Maroon; font-family: Lobster;'>My Sweet Love🥰</h1>",
+        "<h1 style='text-align: center; color: Salmon; font-family: Lobster;'>My Sweet Love🥰</h1>",
         unsafe_allow_html=True
     )
 
